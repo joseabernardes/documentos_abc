b316b4e4f46f5452a79757b9d9469a976f6b2d10
José Bernardes <joseabernardes@gmail.com>
2017-05-12 17:01:13 +0100

    HEADER CORRIGIR

--- partials/_header.php
+++ partials/_header.php
@@ -3,21 +3,21 @@
     <img src="../images/logo.png"  alt="Documentos ABC"/>
     <nav>
         <ul>
-            <li><a href="/v_public/index.php">Inicio</a></li><!--
+            <li><a href="../v_public/index.php">Inicio</a></li><!--
             --><li class="drop">
                 <a href="#">Adicionar Documento</a>
                 <ul>
-                    <li><a href="/v_private/import-document.php">Importar</a></li>
-                    <li><a href="/v_private/create-document.php">Criar</a></li>
+                    <li><a href="../v_private/import-document.php">Importar</a></li>
+                    <li><a href="../v_private/create-document.php">Criar</a></li>
                 </ul>
             </li><!--
-            --><li><a href="/v_private/change-profile.php">Alterar Perfil</a></li><!--
+            --><li><a href="../v_private/change-profile.php">Alterar Perfil</a></li><!--
             <li><a id="nav_login" href="/v_public/authentication.php">Login/Registar</a></li>-->
             <!--
             --><li class="drop" id="nav_login">
                 <a  href="#">José Bernardes</a>
                 <ul>
-                    <li><a href="/v_private/profile-page.php">Perfil</a></li>
+                    <li><a href="../v_private/profile-page.php">Perfil</a></li>
                     <li><a href="#">Sair</a></li>
                 </ul>
 
